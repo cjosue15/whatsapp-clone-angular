3434b99b537c7e96901f3fa4d9ed1bec943111c4
Josue WSL <dev.carlos.morales@gmail.com>
2022-08-07 00:20:58 -0500

    feat: add persisting user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { SocketService } from '@services/socket.service';
+import { Session } from './models/session.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'whatsapp-clone';
+
+  constructor(private _socketService: SocketService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const sessionFromStorage = localStorage.getItem('session');
+    if (sessionFromStorage) {
+      const session = JSON.parse(sessionFromStorage) as Session;
+      this._socketService.addAuthToSocket(session);
+      this._socketService.connect();
+    }
+  }
 }
